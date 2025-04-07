50f931f7cd78d91c3c0860593a6771bd8f568a6e
diegodalr <diegodalr3@gmail.com>
2025-04-07 16:57:00 -0500

    WooCoor:  Scaffold a new plugin Using command npx @wordpress/create-block@latest --variant dynamic

---
+++ apps/woocoor/woocoor-cart/build/woocoor-cart/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'deacffd4050d22093f85');
---
+++ apps/woocoor/woocoor-cart/build/woocoor-cart/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'cebd5950d88c3c41de8b');
